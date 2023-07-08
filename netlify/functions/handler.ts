786b42f57217a026671d7ba6df0682b97d0bdc3d
Dane Miller <khadane.miller@gmail.com>
2023-07-08 01:05:36 -0400

    wrap in try/catch, add proper noti string, edit embed

--- netlify/functions/handler.ts
+++ netlify/functions/handler.ts
@@ -5,7 +5,7 @@ import {
   schedule,
 } from "@netlify/functions";
 
-const headphoneString = "Sony WF-1000XM4";
+const headphoneString = "Sony WH-1000XM4";
 
 const myHandler: Handler = async (
   event: HandlerEvent,
@@ -14,39 +14,47 @@ const myHandler: Handler = async (
   const response = await fetch(
     "https://www.reddit.com/r/bapcsalescanada/.json"
   );
-  const salePosts = await response.json();
+  try {
+    const salePosts = await response.json();
 
-  const sonyHeadphonesPost = salePosts.data.children.find((post) =>
-    post.data.title.includes(headphoneString)
-  );
-
-  console.log(sonyHeadphonesPost);
-
-  const { WEBHOOK_ID, WEBHOOK_TOKEN } = process.env;
-
-  if (sonyHeadphonesPost) {
-    await fetch(
-      `https://discord.com/api/v10/webhooks/${WEBHOOK_ID}/${WEBHOOK_TOKEN}`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          embeds: [
-            {
-              title: sonyHeadphonesPost.data.title,
-              author: sonyHeadphonesPost.data.subreddit_name_prefixed,
-              color: 5814783,
-              url: `https://reddit.com/${sonyHeadphonesPost.data.permalink}`,
-            },
-          ],
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
+    const sonyHeadphonesPost = salePosts.data.children.find((post) =>
+      post.data.title.includes(headphoneString)
     );
 
+    const { WEBHOOK_ID, WEBHOOK_TOKEN } = process.env;
+
+    if (sonyHeadphonesPost) {
+      await fetch(
+        `https://discord.com/api/v10/webhooks/${WEBHOOK_ID}/${WEBHOOK_TOKEN}`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            content: "<@217397769759883265>",
+            embeds: [
+              {
+                title: sonyHeadphonesPost.data.title,
+                color: 5814783,
+                url: `https://reddit.com/${sonyHeadphonesPost.data.permalink}`,
+                author: {
+                  name: sonyHeadphonesPost.data.subreddit_name_prefixed,
+                },
+              },
+            ],
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      return {
+        statusCode: 200,
+      };
+    }
+  } catch (error) {
     return {
-      statusCode: 200,
+      statusCode: 500,
+      body: JSON.stringify({ error }),
     };
   }
 
